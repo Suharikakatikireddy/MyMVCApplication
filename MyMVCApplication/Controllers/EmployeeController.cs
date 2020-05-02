3abb3f58c55667556a6e7e0cb2547a8422e49715
suharikakatiki@gmail.com <suharikakatiki@gmail.com>
2020-05-02 20:02:13 +0100

    Added Edit to the DB

--- MyMVCApplication/Controllers/EmployeeController.cs
+++ MyMVCApplication/Controllers/EmployeeController.cs
@@ -9,18 +9,29 @@ namespace MyMVCApplication.Controllers
 {
     public class EmployeeController : Controller
     {
-        
+         EmployeeContext employeeContext = new EmployeeContext();
         public ActionResult Details()
         {
-
-            EmployeeContext employeeContext = new EmployeeContext();
-            //Employee employee = employeeContext.Employees.FirstOrDefault(e => e.Id == id);
-            return  View(employeeContext.Employees); 
+            
+            return View(employeeContext.Employees);
         }
+        [HttpGet]
         public ActionResult Edit(int id)
         {
-            EmployeeContext employeeContext = new EmployeeContext();
-            Employee employee=employeeContext.Employees.FirstOrDefault()
+            return View(employeeContext.Employees.FirstOrDefault(e => e.Id == id));
+        }
+        [HttpPut]
+        public ActionResult Edit(int id,Employee newEmployee)
+        {            
+            Employee employee = employeeContext.Employees.FirstOrDefault(e=> e.Id == id);
+            employee.Name = newEmployee.Name;
+            employee.Address = newEmployee.Address;
+            employee.Email = newEmployee.Email;
+            employee.Age = newEmployee.Age;
+            employeeContext.SaveChanges();
+
+            return RedirectToAction("Details");
 
         }
+    }
 }
