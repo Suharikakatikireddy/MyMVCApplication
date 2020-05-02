daa816936aa06ac48da5ff228a4d5df646eaeef4
suharikakatiki@gmail.com <suharikakatiki@gmail.com>
2020-05-02 18:22:25 +0100

    This is the master branch for Employee Details

---
+++ MyMVCApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyMVCApplication.Models;
+
+namespace MyMVCApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        
+        public ActionResult Details()
+        {
+
+            EmployeeContext employeeContext = new EmployeeContext();
+            //Employee employee = employeeContext.Employees.FirstOrDefault(e => e.Id == id);
+            return  View(employeeContext.Employees); 
+        }
+        public ActionResult Edit(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee employee=employeeContext.Employees.FirstOrDefault()
+
+        }
+}
